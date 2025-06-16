7b592d31657e2e42ec73404862fb1f98f0b2eabb
Fusidic922 <351099656@qq.com>
2025-06-16 19:09:25 +0800

    1

---
+++ 6 14/6 14/test2.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+//int main()
+//{
+//	187;
+//	3.14;
+//	'w';
+//	"sb";
+//	const int a = 1;
+//	//a = 2;
+//	printf("%d\n",a);
+//	int arr[10] = {};
+//	return 0;
+////}
+//#define MAX "sb"
+//int main()
+//{
+//	printf("%s\n", MAX);
+//
+//	return 0;
+//}
+
+//enum Color 
+//{
+//	RED,
+//	GREEN,
+//	BLUE
+//};
+//
+//int main()
+//{
+//	enum Color c = RED;
+//	printf("%s\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "123456";
+//	char arr2[] = { '1','2','3','4','5','6'};
+//	//int len = strlen("asd");
+//	//printf("%d\n", len);
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	
+//	/*printf("%s\n", arr1);
+//	printf("%s\n", arr2);*/
+//	return 0;
+//}
+
+int main()
+{
+	printf("%c\n",'\'');
+	printf("\"\n");
+	printf("(are you ok\?\?)\n");
+	printf("akjfgfk\tfb\rgdf\n");
+	
+	printf("%d\n", strlen("size of"));
+	printf("%d\n", strlen(":d\test\238\test.c"));
+	return 0;
+}
