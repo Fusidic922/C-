7a92675b2845770e3c7d09347b939e4fb79f8a8f
Fusidic922 <351099656@qq.com>
2025-06-16 21:55:48 +0800

    6 16

---
+++ 6 16/6 16/6 16.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	printf("    **    \n");
+//	printf("    **    \n");
+//	printf("**********\n");
+//	printf("**********\n");
+//	printf("   *   *  \n");
+//	printf("   *   *  \n");
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18     man\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("I lost my phone\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//	printf("%d\n", (-8 + 22) * a - 10 + c / 2);
+//	return 0;
+//}
