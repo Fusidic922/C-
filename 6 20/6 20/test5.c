256bbe0897ccedf9fe077d0077a2002163a718ec
Fusidic922 <351099656@qq.com>
2025-06-20 20:58:17 +0800

    6 20

--- 6 20/6 20/test5.c
+++ 6 20/6 20/test5.c
@@ -61,18 +61,74 @@
 //	return 0;
 //}
 
-void Judge(int x)
-{
-	if (x % 2 == 1)
-		printf("%d ", x);
-}
+//void Judge(int x)
+//{
+//	if (x % 2 == 1)
+//		printf("%d ", x);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 100)
+//	{
+//		i++;
+//		Judge(i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期三\n");
+//		break;
+//	case 4:
+//		printf("星期四\n");
+//		break;
+//	case 5:
+//		printf("星期五\n");
+//		break;
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期天\n");
+//		break;
+//	}
+//	return 0;
+//}
+
 int main()
 {
-	int i = 0;
-	while (i < 100)
+	int day = 0;
+	scanf("%d", &day);
+	switch (day)
 	{
-		i++;
-		Judge(i);
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("weekday\n");
+		break;
+	case 6:
+	case 7:
+		printf("weekend\n");
+		break;
+	default:
+		printf("错误\n");
+		break;
 	}
+
 	return 0;
 }
